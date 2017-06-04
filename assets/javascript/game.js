06ac2dacf00cc1312874983079e75a6f7fcddf8e
Brayden Fortino <Brayden@tests-MacBook-Pro.local>
2017-06-04 18:56:39 -0400

    notes and minor edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
-window.onload = function () {
+window.onload = function () { //Will run once window is loaded
 
-  //Array of letters in the alphabet-these will be button options for players to guess from
+  //Globals
+
+  //Array of letters in the alphabet-these will be the button options for players to guess from
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  var categories;         // Arrays of words for comp to choose from
+  var categories;         // Arrays of categories to choose from
   var chosenCategory;     // Selected catagory
-  var word ;              // Slected random word from categories
+  var word ;              // Slected random word from chosenCategories
   var guess ;             // Geuss
   var geusses = [ ];      // Empty array to store player geusses
   var lives ;             // Lives
@@ -15,50 +17,52 @@ window.onload = function () {
   var space;              // Number of spaces in word '-'
 
   // Get elements
-  var showLives = document.getElementById("player-lives");
+  //var showLives = document.getElementById("player-lives");
   var showCatagory = document.getElementById("scatagory");
 
 
 
   //Function creates alphabet buttons ul
   var buttons = function () {
-    gameButtons = document.getElementById('buttons'); //Grab div id="buttons"
-    letters = document.createElement('ul');
+    gameButtons = document.getElementById('buttons'); //Grab div id="buttons"-gameButtons = local var
+    letters = document.createElement('ul'); //Create ul element in div id="buttons"
 
     for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      gameButtons.appendChild(letters);
-      letters.appendChild(list);
+      letters.setAttribute("id", "alphabet"); //Giv ul id="alphabet"
+      //letters.id = 'alphabet';
+      list = document.createElement('li'); //Create li inside of ul
+      list.setAttribute("id", "letter"); //Give li id="letter"
+      //list.id = 'letter';
+      list.innerHTML = alphabet[i]; //Enter letters of [alphabet] into HTML (li element created above)
+      buttonClick(); //Call buttonClick function ()
+      gameButtons.appendChild(letters); //Put ul in parent div id="buttons"
+      letters.appendChild(list); //Put li in parent div id="alphabet"
     }
   }
 
 
-  //Catagory to select from
+  //Catagories to select from-Game loop will select random word from array of categories
   var selectCat = function () {
     if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = "The Chosen Category Is Star Wars Locations";
+      catagoryName.innerHTML = "Star Wars Locations";
     } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = "The Chosen Category Is Star Wars Characters";
+      catagoryName.innerHTML = "Star Wars Characters";
     } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = "The Chosen Category Is Star Wars Space Ships";
+      catagoryName.innerHTML = "Star Wars Space Ships";
     }
   }
 
   // Create geusses ul
    result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
+    wordHolder = document.getElementById('hold'); //Grabs div id="hold"
+    correct = document.createElement('ul'); //Creates ul element
 
     for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
+      correct.setAttribute('id', 'my-word'); //Give ul id="my-word"
+      guess = document.createElement('li'); //Create li element
+      guess.setAttribute('class', 'guess'); //Give li id="guess"
       if (word[i] === "-") {
-        guess.innerHTML = " ";      //If the [i] in the word is "-"(space) then enter space into hmtl or else its "_"
+        guess.innerHTML = " "; //If the [i] in the word is "-"(space) then enter space into hmtl or else its "_"
         space = 1; //Assigns value of 1 to var space
       } else {
         guess.innerHTML = "_";
@@ -70,10 +74,10 @@ window.onload = function () {
     }
   }
 
-
   //Player lives
    comments = function () {
 
+     //Arrays of random comments for win/lose/half-lives
      var winQuotes = ["The Force is Strong With You!", "Impressive. Most Impressive. Obi-Wan has Taught You Well"];
      var winComment = winQuotes[Math.floor(Math.random() * winQuotes.length)];
 
@@ -82,6 +86,9 @@ window.onload = function () {
 
      var halfQuotes = ["Do or Do Not. There is No Try", "Use the Force", "Patience Young Padawan", "Never Tell Me the Odds"]
      var halfComment = halfQuotes[Math.floor(Math.random() * halfQuotes.length)];
+
+
+     var showLives = document.getElementById("player-lives");
 
     showLives.innerHTML = "You have " + lives + " lives";
     //Lose
@@ -102,10 +109,10 @@ window.onload = function () {
 
 
   // OnClick Function
-   check = function () {
+   buttonClick = function () {
     list.onclick = function () {
       var geuss = (this.innerHTML);
-      this.setAttribute("class", "active");
+      this.setAttribute("class", "active"); //
       this.onclick = null;
       for (var i = 0; i < word.length; i++) {
         if (word[i] === geuss) {
@@ -123,6 +130,12 @@ window.onload = function () {
       }
     }
   }
+
+  // KeyPress Function
+  //keyPress = function () { same as buttonClick but keyboard; maybe document.onkeyup = function(event) {}?
+
+
+
 
 
   //Game loop
@@ -153,8 +166,7 @@ window.onload = function () {
   play();
 
 
-   //Reset function-once button is clicked =
-
+   //Reset function-once button is clicked
    document.getElementById('reset').onclick = function() {
     correct.parentNode.removeChild(correct);
     letters.parentNode.removeChild(letters);
